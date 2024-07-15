c7756fba8c1b9e01399947689e37841602bb8683
Prabuddha747 <prabuddha331@gmail.com>
2024-07-15 21:26:56 +0530

    Crowd_funding based page to ease the life of commoners

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    context ={
+        'variable':"this is sent"
+    }
+    return render(request, 'index.html',context)
+    # return HttpResponse("this is homepage")
+
+
+def about(request):
+    return HttpResponse("this is about")
+
+
+def services(request):
+   return HttpResponse("this is services")
+
+def contacts(request):
+   return HttpResponse("this is contacts")
